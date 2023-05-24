2a27808af3c222b56c932f03b21dd1d8260380cd
chnoma <85920821+chnoma@users.noreply.github.com>
2023-05-24 00:44:29 -0500

    Added delete functionality

--- index.js
+++ index.js
@@ -20,7 +20,62 @@ module.context.use(router);
 
 // TODO: Access validation accross the board
 
-// Posts
+// --------------------- Posts ---------------------
+
+// Retrieve
+router.get('posts/get/:id', function (req, res) {
+    try {
+        const data = col_posts.document(req.pathParams.id);
+        res.send(data);
+    }
+    catch (e) {
+        if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
+            throw e;
+        }
+        res.throw(404, 'Post does not exist');
+    }
+})
+.pathParam('id', joi.string().required(), 'Post ID')
+.response(joi.object().required(), 'Post Data')
+.summary('Retrieve a post by ID')
+.description('Retrieves a post by ID');
+
+
+// Delete
+router.post('posts/delete', function (req, res) {
+    const body = req.body;
+    
+    let post;
+    try {
+        post = col_posts.document(body.post_id);
+    }
+    catch (e) {
+        if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
+            throw e;
+        }
+        res.throw(404, 'Post does not exist');
+    }
+
+    const query = `
+    let post = document(@post_id)
+    for v, e in 1..1
+    ANY
+    post posted_by
+    remove e in posted_by
+    remove post in posts
+    `;
+
+    const params = { post_id: post._id};
+
+    const result = db._query(query, params).toArray()
+
+    res.send({"status": "200"})
+})
+.body(joi.object({
+    post_id: joi.string().required()
+}).required(), 'Post ID')
+.summary('Delete a post')
+.description("Deletes a post with a given ID, and its related connections.");
 
 // New
 router.post('posts/create', function (req, res) {
@@ -61,22 +116,3 @@ router.post('posts/create', function (req, res) {
 }).required(), 'Resulting post ID')
 .summary('Create a new post')
 .description("Creates a new post with the supplied body under the supplied user's profile.");
-
-
-// Retrieve
-router.get('posts/get/:id', function (req, res) {
-    try {
-        const data = col_posts.document(req.pathParams.id);
-        res.send(data);
-    }
-    catch (e) {
-        if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
-            throw e;
-        }
-        res.throw(404, 'Post does not exist');
-    }
-})
-.pathParam('id', joi.string().required(), 'Post ID')
-.response(joi.object().required(), 'Post Data')
-.summary('Retrieve a post by ID')
-.description('Retrieves a post by ID');
