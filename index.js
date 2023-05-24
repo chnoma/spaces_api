5e107db49bd936794754228584a41f6cb1a9466c
chnoma <85920821+chnoma@users.noreply.github.com>
2023-05-23 23:39:04 -0500

    Added basic post creation

--- index.js
+++ index.js
@@ -9,13 +9,54 @@ const col_posts = db._collection('posts');
 const col_users = db._collection('users');
 const col_spaces = db._collection('spaces');
 
+const edge_posted_by = db._collection('posted_by');
+const edge_follows = db._collection('follows');
+const edge_space_structure = db._collection('space_structure');
+const edge_likes = db._collection('likes');
+const edge_post_space = db._collection('post_space');
+
 const router = createRouter();
 module.context.use(router);
 
 // TODO: Access validation accross the board
 
 // Posts
-router.get('posts/:id', function (req, res) {
+
+// New
+router.post('posts/create', function (req, res) {
+    const body = req.body;
+    
+    let user;
+    try {
+        user = col_users.document(body.user_id);
+    }
+    catch (e) {
+        if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
+            throw e;
+        }
+        res.throw(404, 'User does not exist');
+    }
+    
+    const post = col_posts.save( {body: body.post_body} );
+
+    edge_posted_by.save({ _from: post._id, _to: user._id });
+
+    res.send({post_id: post._id});
+
+})
+.body(joi.object({
+    post_body: joi.string().required(),
+    user_id: joi.string().required()
+}).required(), 'Post body and user ID')
+.response(joi.object({
+    post_id: joi.string().required()
+}).required(), 'Resulting post ID')
+.summary('Create a new post')
+.description("Creates a new post with the supplied body under the supplied user's profile.");
+
+
+// Retrieve
+router.get('posts/get/:id', function (req, res) {
     try {
         const data = col_posts.document(req.pathParams.id);
         res.send(data);
@@ -24,7 +65,7 @@ router.get('posts/:id', function (req, res) {
         if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
             throw e;
         }
-        res.throw(404, 'post does not exist');
+        res.throw(404, 'Post does not exist');
     }
 })
 .pathParam('id', joi.string().required(), 'Post ID')
